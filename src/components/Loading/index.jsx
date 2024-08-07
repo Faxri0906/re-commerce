6d66f3ab5d3015ed440a8728ebe105da9dd0d710
Fakhri Mammedzade <safm11060917@gmail.com>
2024-08-07 15:00:04 +0400

    site

---
+++ src/components/Loading/index.jsx
@@ -0,0 +1,8 @@
+
+export default function Loading() {
+  return (
+    <div className="flex items-center justify-center">
+        <div className="w-96 h-96 border-8 rounded-full border-black/80 animate-spin border-r-black/20 justify-center items-center mt-auto"></div>
+    </div>
+  );
+};
